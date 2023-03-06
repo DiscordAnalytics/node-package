8970efd5eb6a94cb6886ce0c78422b2dab5764a7
ValRedstone <valdesign.dev@gmail.com>
2023-03-06 19:18:36 +0100

    indented code

--- lib/src/DiscordAnalytics.ts
+++ lib/src/DiscordAnalytics.ts
@@ -9,7 +9,6 @@ import { EventsToTrack, LibType, ErrorCodes, ApiEndpoints } from '../utils/types
  * @param {LibType} type The type of client to track.
  * @param {EventsToTrack} eventsToTrack The events to track.
  * @example
- * // Using Discord.js
  * const DiscordAnalytics, { LibType } = require('discord-analytics');
  * const { Client } = require('discord.js');
  * const client = new Client();
@@ -25,127 +24,113 @@ import { EventsToTrack, LibType, ErrorCodes, ApiEndpoints } from '../utils/types
  *   da.trackEvents();
  * });
  * client.login('token');
- * 
- * // Using Eris
- * const DiscordAnalytics, { LibType } = require('discord-analytics');
- * const Eris = require('eris');
- * const client = new Eris('token');
- * const da = new DiscordAnalytics(client, LibType.ERIS, {
- *   trackInteractions: true,
- *   trackMessageCreate: true,
- *   trackMessageDelete: true,
- *   trackGuildDelete: true,
- *   trackGuildCreate: true,
- *   trackUserCount: true
- * });
- * client.on('ready', () => {
- *  da.trackEvents();
- * });
- * client.connect();
  */
 export default class DiscordAnalytics {
-    private _client: DJSClient | ErisClient;
-    private _eventsToTrack: EventsToTrack;
+  private readonly _client: DJSClient | ErisClient;
+  private _eventsToTrack: EventsToTrack;
 
-    constructor(client: DJSClient | ErisClient, type: LibType, eventsToTrack: EventsToTrack) {
-        if (type === LibType.DJS && client instanceof DJSClient) this._client = client;
-        if (type === LibType.ERIS && client instanceof ErisClient) this._client = client;
-        else throw new Error(ErrorCodes.INVALID_CLIENT_TYPE);
+  constructor(client: DJSClient | ErisClient, type: LibType, eventsToTrack: EventsToTrack) {
+    if (type === LibType.DJS && client instanceof DJSClient) this._client = client;
+    if (type === LibType.ERIS && client instanceof ErisClient) this._client = client;
+    else throw new Error(ErrorCodes.INVALID_CLIENT_TYPE);
 
-        this._eventsToTrack = eventsToTrack;
+    this._eventsToTrack = eventsToTrack;
+  }
+
+  /**
+   * @description The client to track.
+   * @type {DJSClient | ErisClient}
+   * @readonly
+   */
+  public get client(): DJSClient | ErisClient {
+      return this._client;
+  }
+
+  /**
+   * @description Track the events specified in the constructor options.
+   * @returns {void}
+   */
+  public trackEvents(): void {
+    if (this._client instanceof DJSClient) {
+      if (!this._client.isReady()) throw new Error(ErrorCodes.CLIENT_NOT_READY);
+      else this.trackDJSEvents();
     }
-
-    /**
-     * @description The client to track.
-     * @type {DJSClient | ErisClient}
-     * @readonly
-     */
-    public get client(): DJSClient | ErisClient {
-        return this._client;
+    else if (this._client instanceof ErisClient) {
+      if (!this._client.ready) throw new Error(ErrorCodes.CLIENT_NOT_READY);
+      else this.trackErisEvents();
     }
+    else throw new Error(ErrorCodes.INVALID_CLIENT_TYPE);
+  }
 
-    /**
-     * @description Track the events specified in the constructor options.
-     * @returns {void}
-     */
-    public trackEvents(): void {
-        if (this._client instanceof DJSClient) {
-            if (!this._client.isReady()) throw new Error(ErrorCodes.CLIENT_NOT_READY);
-            else this.trackDJSEvents();
-        }
-        if (this._client instanceof ErisClient) {
-            if (!this._client.ready) throw new Error(ErrorCodes.CLIENT_NOT_READY);
-            else this.trackErisEvents();
-        }
+  private trackDJSEvents(): void {
+    if (this._client instanceof DJSClient) {
+      if (this._eventsToTrack.trackInteractions) {
+        this._client.on('interactionCreate', (interaction) => {
+          fetch(`${ApiEndpoints.BASE_URL}${ApiEndpoints.TRACK_URL}${ApiEndpoints.ROUTES.INTERACTIONS}`, {
+            method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: this._eventsToTrack.trackUserCount ?
+                this._eventsToTrack.trackUserLanguage ?
+                  JSON.stringify({
+                    id: interaction.id,
+                    type: interaction.type,
+                    user: {
+                      id: interaction.user.id,
+                      locale: interaction.locale,
+                      bot: interaction.user.bot
+                    },
+                    guild: interaction.guild ? {
+                      id: interaction.guild.id,
+                      memberCount: interaction.guild.memberCount
+                    } : null
+                  }) :
+                  JSON.stringify({
+                    id: interaction.id,
+                    type: interaction.type,
+                    user: {
+                      id: interaction.user.id,
+                      bot: interaction.user.bot
+                    },
+                    guild: interaction.guild ? {
+                      id: interaction.guild.id,
+                      memberCount: interaction.guild.memberCount
+                    } : null
+                  }) :
+                  this._eventsToTrack.trackUserLanguage ?
+                    JSON.stringify({
+                      id: interaction.id,
+                      type: interaction.type,
+                      user: {
+                        id: interaction.user.id,
+                        locale: interaction.locale,
+                        bot: interaction.user.bot
+                      },
+                      guild: interaction.guild ? {
+                        id: interaction.guild.id
+                      } : null
+                    }) :
+                    JSON.stringify({
+                      id: interaction.id,
+                      type: interaction.type,
+                      user: {
+                        id: interaction.user.id,
+                        bot: interaction.user.bot
+                      },
+                      guild: interaction.guild ? {
+                        id: interaction.guild.id
+                      } : null
+                    })
+          }).then(r => {
+            if (r.status !== 200) throw new Error(ErrorCodes.INVALID_RESPONSE);
+          });
+        });
+      }
     }
+  }
 
-    private trackDJSEvents(): void {
-        if (this._client instanceof DJSClient) {
-            if (this._eventsToTrack.trackInteractions) {
-                this._client.on('interactionCreate', (interaction) => {
-                    fetch(`${ApiEndpoints.BASE_URL}${ApiEndpoints.TRACK_URL}${ApiEndpoints.ROUTES.INTERACTIONS}`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: this._eventsToTrack.trackUserCount ?
-                            this._eventsToTrack.trackUserLanguage ?
-                                JSON.stringify({
-                                    id: interaction.id,
-                                    type: interaction.type,
-                                    user: {
-                                        id: interaction.user.id,
-                                        locale: interaction.locale,
-                                        bot: interaction.user.bot
-                                    },
-                                    guild: interaction.guild ? {
-                                        id: interaction.guild.id,
-                                        memberCount: interaction.guild.memberCount
-                                    } : null
-                                }) :
-                                JSON.stringify({
-                                    id: interaction.id,
-                                    type: interaction.type,
-                                    user: {
-                                        id: interaction.user.id,
-                                        bot: interaction.user.bot
-                                    },
-                                    guild: interaction.guild ? {
-                                        id: interaction.guild.id,
-                                        memberCount: interaction.guild.memberCount
-                                    } : null
-                                }) :
-                            this._eventsToTrack.trackUserLanguage ?
-                                JSON.stringify({
-                                    id: interaction.id,
-                                    type: interaction.type,
-                                    user: {
-                                        id: interaction.user.id,
-                                        locale: interaction.locale,
-                                        bot: interaction.user.bot
-                                    },
-                                    guild: interaction.guild ? {
-                                        id: interaction.guild.id
-                                    } : null
-                                }) :
-                                JSON.stringify({
-                                    id: interaction.id,
-                                    type: interaction.type,
-                                    user: {
-                                        id: interaction.user.id,
-                                        bot: interaction.user.bot
-                                    },
-                                    guild: interaction.guild ? {
-                                        id: interaction.guild.id
-                                    } : null
-                                })
-                    });
-                });
-            }
-        }
-    }
-
-    private trackErisEvents(): void {
-        console.log('Eris events are not yet supported.');
-    }
+  private trackErisEvents(): void {
+    console.log('Eris events are not yet supported.');
+  }
 }
--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -1,33 +1,34 @@
 export enum LibType {
-    DJS,
-    ERIS
+  DJS,
+  ERIS
 }
 
 export interface EventsToTrack {
-    trackInteractions: boolean;
-    trackMessageCreate: boolean;
-    trackMessageDelete: boolean;
-    trackGuildDelete: boolean;
-    trackGuildCreate: boolean;
-    trackUserCount: boolean;
-    trackUserLanguage: boolean;
+  trackInteractions: boolean;
+  trackMessageCreate: boolean;
+  trackMessageDelete: boolean;
+  trackGuildDelete: boolean;
+  trackGuildCreate: boolean;
+  trackUserCount: boolean;
+  trackUserLanguage: boolean;
 }
 
 export const ErrorCodes = {
-    INVALID_CLIENT_TYPE: 'Invalid client type, please use a valid client.',
-    CLIENT_NOT_READY: 'Client is not ready, please start the client first.'
+  INVALID_CLIENT_TYPE: 'Invalid client type, please use a valid client.',
+  CLIENT_NOT_READY: 'Client is not ready, please start the client first.',
+  INVALID_RESPONSE: 'Invalid response from the API, please try again later.',
 }
 
 export const ApiEndpoints = {
-    BASE_URL: 'https://discord-analytics.com/api', // TODO: Change the URL when we have a domain
-    TRACK_URL: '/track',
-    ROUTES: {
-        INTERACTIONS: '/interactions',
-        MESSAGE_CREATE: '/message/create',
-        MESSAGE_DELETE: '/message/delete',
-        GUILD_CREATE: '/guild/create',
-        GUILD_DELETE: '/guild/delete',
-        USER_COUNT: '/user/count',
-        USER_LANGUAGE: '/user/language'
-    }
+  BASE_URL: 'https://discord-analytics.com/api', // TODO: Change the URL when we have a domain
+  TRACK_URL: '/track',
+  ROUTES: {
+    INTERACTIONS: '/interactions',
+    MESSAGE_CREATE: '/message/create',
+    MESSAGE_DELETE: '/message/delete',
+    GUILD_CREATE: '/guild/create',
+    GUILD_DELETE: '/guild/delete',
+    USER_COUNT: '/user/count',
+    USER_LANGUAGE: '/user/language'
+  }
 }

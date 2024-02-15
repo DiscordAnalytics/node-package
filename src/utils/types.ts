593069acc8b0f5db78668b164bc6d9e8d22c875e
Nolhan <nolhan.dev@gmail.com>
2024-02-15 18:10:31 +0100

    Global files update

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,7 +6,7 @@ export interface EventsToTrack {
   trackGuildsLocale: boolean;
 }
 export const ApiEndpoints = {
-  BASE_URL: 'https://discordanalytics.xyz/api',
+  BASE_URL: 'https://beta.discordanalytics.xyz/api',
   EDIT_SETTINGS_URL: '/bots/:id',
   EDIT_STATS_URL: '/bots/:id/stats',
 }
@@ -18,7 +18,8 @@ export const ErrorCodes = {
   INVALID_API_TOKEN: 'Invalid API token, please get one at ' + ApiEndpoints.BASE_URL.split('/api')[0] + ' and try again.',
   DATA_NOT_SENT: "Data cannot be sent to the API, I will try again in a minute.",
   SUSPENDED_BOT: "Your bot has been suspended, please check your mailbox for more information.",
-  INVALID_EVENTS_COUNT: "Invalid events count. Please enable at least one event to track.",
+  INSTANCE_NOT_INITIALIZED: "It seem that you didn't initialize your instance. Please check the docs for more informations.",
+  INVALID_EVENTS_COUNT: "invalid events count"
 }
 
 export type Locale = 'id' | 'en-US' | 'en-GB' | 'bg' | 'zh-CN' | 'zh-TW' | 'hr' | 'cs' | 'da' | 'nl' | 'fi' | 'fr' | 'de' | 'el' | 'hi' | 'hu' | 'it' | 'ja' | 'ko' | 'lt' | 'no' | 'pl' | 'pt-BR' | 'ro' | 'ru' | 'es-ES' | 'sv-SE' | 'th' | 'tr' | 'uk' | 'vi';
@@ -29,4 +30,11 @@ export enum InteractionType {
   MessageComponent,
   ApplicationCommandAutocomplete,
   ModalSubmit,
+}
+
+export interface DiscordAnalyticsOptions {
+  client: any;
+  apiToken: string;
+  sharded?: boolean;
+  debug?: boolean;
 }

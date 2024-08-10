51ff80f3682b2d911089a2cdfcd51e06450ab512
FAYシ <103030954+FAYStarNext@users.noreply.github.com>
2024-08-10 11:43:02 +0700

    Added Discord.JS-Light

---
+++ src/test/discordjs-light/shards.ts
@@ -0,0 +1,10 @@
+import { ShardingManager } from 'discord.js-light';
+import {config} from "dotenv";
+
+config()
+
+const manager = new ShardingManager('./dist/src/test/discordjs/index.js', { token: process.env.DISCORD_TOKEN });
+
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+
+manager.spawn();

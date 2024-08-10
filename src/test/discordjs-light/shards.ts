0f0f4d7dfbd4189378d7c113b64478e039200f86
FAYシ <103030954+FAYStarNext@users.noreply.github.com>
2024-08-10 15:54:52 +0700

    chore: Update import statement for DiscordAnalytics in discordjs-light test file

--- src/test/discordjs-light/shards.ts
+++ src/test/discordjs-light/shards.ts
@@ -3,7 +3,7 @@ import {config} from "dotenv";
 
 config()
 
-const manager = new ShardingManager('./dist/src/test/discordjs/index.js', { token: process.env.DISCORD_TOKEN });
+const manager = new ShardingManager('./dist/src/test/discordjs-light/index.js', { token: process.env.DISCORD_TOKEN });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
